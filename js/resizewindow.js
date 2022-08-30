c922e25c6bd22a36fbf4739de6b33161b52e4bda
laczoandras <laczoandras@gmail.com>
2022-08-30 15:21:17 +0200

    16 commit

--- js/resizewindow.js
+++ js/resizewindow.js
@@ -1,12 +1,16 @@
 window.addEventListener("resize", resizeWindow);
 
+let oldSize=window.innerWidth;
+
 function resizeWindow() {
-    if (window.innerWidth>=768) {
+    
+
+    if (window.innerWidth>=768 && oldSize<768) {
         let toShow = document.querySelectorAll(".m-texts, .latest-projects, .technologies");
         toShow.forEach((element)=> element.style.display="block")
         toShow.forEach((element)=> element.style.opacity="1")
     }
-    if (window.innerWidth<768) {
+    if (window.innerWidth<768 && oldSize>768) {
         let toNotShow = document.querySelectorAll(".latest-projects, .technologies");
         toNotShow.forEach((element)=> element.style.display="none")
         toNotShow.forEach((element)=> element.style.opacity="0")
@@ -16,5 +20,6 @@ function resizeWindow() {
         toShow.style.opacity="1"
         toShow.setAttribute("menuVisible","true")
     }
+    oldSize=window.innerWidth;
 
 }
